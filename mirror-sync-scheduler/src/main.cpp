0eea85a4682b9448c3c0b960d9a85eadc8bf5899
carykees98 <trainboy980@gmail.com>
2025-02-12 22:49:51 -0500

    add ability to load logging level from environment variable

--- mirror-sync-scheduler/src/main.cpp
+++ mirror-sync-scheduler/src/main.cpp
@@ -4,11 +4,16 @@
  * @brief
  */
 
+// Third Party Includes
+#include <spdlog/cfg/env.h>
+
 // Project Includes
 #include <mirror/sync_scheduler/SyncScheduler.hpp>
 
 auto main() -> int
 {
+    spdlog::cfg::load_env_levels();
+
     mirror::sync_scheduler::SyncScheduler syncScheduler {};
 
     syncScheduler.run();
